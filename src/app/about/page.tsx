f395b4b66e026a07e479b24bb6ef3ea6490302e4
Resheph Inayat <rasafinayat@gmail.com>
2024-11-23 15:04:51 +0500

    Added Seperate file for About Us Data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,40 +6,9 @@ import { MdEmail } from "react-icons/md";
 // import { motion } from "framer-motion";
 
 import { TeamCard } from "@/components/TeamCard";
+import { aboutData } from "@/data/AboutUs";
 
-// Data for the page
-const aboutData = {
-  header: {
-    title: "Divisional Leaders",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore culpa assumenda, minima maxime alias porro ipsa necessitatibus laborum magni natus, id tempora optio deleniti voluptate earum nisi dolorem corporis unde.",
-    leaders: [
-      {
-        name: "Major Imran George ( DC )",
-        title: "Divisional Coordinator (DC)",
-      },
-      {
-        name: "Major Lubna Imran ( DDWM )",
-        title: "Divisional Director of Women's Ministry (DDWM)",
-      },
-    ],
-  },
-  team: [
-    {
-      name: "Major Joseph Masih",
-      title: "Divisional Business Officer (DBO)",
-      socialLink: "https://www.facebook.com/tsa.pakistan",
-      contact: "0302-1234567",
-    },
-    {
-      name: "Major Zaheeda",
-      title: "Divisional Finance Officer (DFO)",
-      socialLink: "https://www.facebook.com/tsa.pakistan",
-      contact: "0302-1234567",
-    },
-  ],
-  image: "/Leaders.jpg",
-};
+
 
 const AboutPage = () => {
   return (
---
+++ src/data/AboutUs.ts
@@ -0,0 +1,32 @@
+export const aboutData = {
+  header: {
+    title: "Divisional Leaders",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore culpa assumenda, minima maxime alias porro ipsa necessitatibus laborum magni natus, id tempora optio deleniti voluptate earum nisi dolorem corporis unde.",
+    leaders: [
+      {
+        name: "Major Imran George ( DC )",
+        title: "Divisional Coordinator (DC)",
+      },
+      {
+        name: "Major Lubna Imran ( DDWM )",
+        title: "Divisional Director of Women's Ministry (DDWM)",
+      },
+    ],
+  },
+  team: [
+    {
+      name: "Major Joseph Masih",
+      title: "Divisional Business Officer (DBO)",
+      socialLink: "https://www.facebook.com/tsa.pakistan",
+      contact: "0302-1234567",
+    },
+    {
+      name: "Major Zaheeda",
+      title: "Divisional Finance Officer (DFO)",
+      socialLink: "https://www.facebook.com/tsa.pakistan",
+      contact: "0302-1234567",
+    },
+  ],
+  image: "/Leaders.jpg",
+};
